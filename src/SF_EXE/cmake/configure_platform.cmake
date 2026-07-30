fe1360fdcc39ba6461fadd7063556bab97f70260
Trulio <tulio-santos@hotmail.com>
2026-07-30 17:55:47 -0300

    add switch cmake script

--- src/SF_EXE/cmake/configure_platform.cmake
+++ src/SF_EXE/cmake/configure_platform.cmake
@@ -2,6 +2,9 @@ function(osf_configure_platform target)
   if(EMSCRIPTEN)
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/platforms/Emscripten.cmake")
     osf_configure_emscripten_platform(${target})
+  elseif(NINTENDO_SWITCH)
+    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/platforms/Switch.cmake")
+    osf_configure_switch_platform(${target})
   elseif(
       WIN32
       OR APPLE
