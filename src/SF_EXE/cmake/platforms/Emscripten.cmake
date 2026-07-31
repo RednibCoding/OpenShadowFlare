34ea1e3ccd63379850827fc961ac741bdcc476da
Trulio <tulio-santos@hotmail.com>
2026-07-31 11:12:21 -0300

    Update ems build to use the same index page

--- src/SF_EXE/cmake/platforms/Emscripten.cmake
+++ src/SF_EXE/cmake/platforms/Emscripten.cmake
@@ -21,19 +21,40 @@ function(osf_configure_emscripten_platform target)
   )
 
   set(web_shell
-    "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../web/index.html")
+    "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../../../gh-pages")
   set(web_shell_output "${CMAKE_CURRENT_BINARY_DIR}/index.html")
   add_custom_command(
     OUTPUT "${web_shell_output}"
     COMMAND
       ${CMAKE_COMMAND} -E copy
-      "${web_shell}" "${web_shell_output}"
-    DEPENDS "${web_shell}"
-    COMMENT "Copying OpenShadowFlare web shell"
+      "${web_shell}/index.html" "${CMAKE_CURRENT_BINARY_DIR}/index.html"
+    COMMAND
+      ${CMAKE_COMMAND} -E copy
+      "${web_shell}/app.js" "${CMAKE_CURRENT_BINARY_DIR}/app.js"
+    COMMAND
+      ${CMAKE_COMMAND} -E copy
+      "${web_shell}/style.css" "${CMAKE_CURRENT_BINARY_DIR}/style.css"
+    DEPENDS
+      "${web_shell}/index.html"
+      "${web_shell}/app.js"
+      "${web_shell}/style.css"
+    COMMENT "Copying OpenShadowFlare web shell from gh-pages"
   )
   add_custom_target(
     shadowflare_web_shell ALL
     DEPENDS "${web_shell_output}"
   )
   add_dependencies(shadowflare_web_shell ${target})
+
+  set(web_assets_dir "${CMAKE_CURRENT_BINARY_DIR}/assets")
+  add_custom_command(
+    TARGET ${target} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${web_assets_dir}"
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      "$<TARGET_FILE:${target}>" "${web_assets_dir}/ShadowFlare_rebuilt.js"
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      "$<TARGET_FILE_DIR:${target}>/ShadowFlare_rebuilt.wasm"
+      "${web_assets_dir}/ShadowFlare_rebuilt.wasm"
+    COMMENT "Copying WebAssembly artifacts to assets/"
+  )
 endfunction()
